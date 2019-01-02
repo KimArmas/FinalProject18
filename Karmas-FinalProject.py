bf6815324325dd41ccf0ce44208ed612945e78fe
karmas <karmas@ucvts.org>
2019-01-02 14:41:32 +0000

    Preliminary Approach

---
+++ Karmas-FinalProject.py
@@ -0,0 +1,17 @@
+import random
+
+print("Welcome to my game!")
+
+username=input("Please enter your name user\n")
+print(f"The rules are simple {username}! You will be presented with mini challenges, kind of like a carnival.")
+
+idea board=
+20 questions
+get info on every student in grade, make them answer certain questions,
+create database then have people play 20 questions where the ocmputer has to quess who they are based on the responses they give to the computer
+
+computer chooses a noun, and you have qa certain number of guesses to figure out what it is,
+and you can only ask yes or no questions
+
+
+may 15
